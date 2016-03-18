e35ed0ccffdc434c38139fe0304cce8faca03b8b
Toko Dougherty <toko.dougherty@gmail.com>
2016-03-18 05:21:44 +0000

    Basic framework/scaffolding of ruby app.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  #back end for pages/index
+  def index
+  end
+  #back end for pages/home
+  def home
+  end
+  #back end for pages/profile
+  def profile
+  end
+  #back end for pages/explore
+  def explore
+  end
+end
